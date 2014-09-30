5d5e31d6e6829ab4750079b7c6a3f7f300228d3b
shoobah <shoobah@gmail.com>
2014-09-30 23:51:05 +0200

    First

---
+++ app/router.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+var Router = Ember.Router.extend({
+  location: SmaktestENV.locationType
+});
+
+Router.map(function() {
+});
+
+export default Router;
---
+++ tests/helpers/resolver.js
@@ -0,0 +1,9 @@
+import Resolver from 'ember/resolver';
+
+var resolver = Resolver.create();
+
+resolver.namespace = {
+  modulePrefix: 'smaktest'
+};
+
+export default resolver;
