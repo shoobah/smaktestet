9f22397f8c4dc43ba6cba37b0f214c5a6b0bab98
shoobah <shoobah@gmail.com>
2014-10-14 23:13:45 +0200

    Fixat till lite

--- app/app.js
+++ app/app.js
@@ -6,7 +6,8 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
   modulePrefix: 'smaktest', // TODO: loaded via config
-  Resolver: Resolver
+  Resolver: Resolver,
+  LOG_TRANSITIONS: true
 });
 
 loadInitializers(App, 'smaktest');
--- app/routes/temperature.js
+++ app/routes/temperature.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model: function(){
-		return {'items':['silly','bugger']};
-	}
+    model: function() {
+        return {
+            'items': ['silly', 'bugger']
+        };
+    }
 });
